98b05d29ef0f42dbf15667f4f96dd8bd2b374f0d
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-11-27 21:28:45 +0100

    add js and css to checkout to fix stripe elements

--- tarh_tastyhub/settings.py
+++ tarh_tastyhub/settings.py
@@ -155,7 +155,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
-STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WH_SECRET')  # FIXED mismatch
-DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WH_SECRET')
+DEFAULT_FROM_EMAIL = 'tarhtastyhub@tarh_tastyhuh.com'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
