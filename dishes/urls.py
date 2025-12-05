7cc8a26124d07fb53f613754fc68a23e5fa94460
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-05 10:32:43 +0100

    fix search.js to submit search enter key well

--- dishes/urls.py
+++ dishes/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.all_dishes, name='dish_list'),
     path('category/<slug:category_slug>/', views.dish_list_by_category, name='dish_list_by_category'),
     path('<slug:slug>/', views.dish_detail, name='dish_detail'),
-    path('search-dishes/', views.search_dishes, name='search_dishes'),
     path('add/', views.add_dish, name='add_dish'),
     path('edit/<slug:slug>/', views.edit_dish, name='edit_dish'),
     path('delete/<slug:slug>/', views.delete_dish, name='delete_dish'),
