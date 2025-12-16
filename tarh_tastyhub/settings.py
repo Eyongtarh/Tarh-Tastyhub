221575527134443003898cbfaa20697e5ce632d4
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-16 19:06:40 +0100

    change debug to True

--- tarh_tastyhub/settings.py
+++ tarh_tastyhub/settings.py
@@ -11,7 +11,7 @@ from django.contrib.messages import constants as messages
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Debug
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True
 
 # Allowed hosts
 ALLOWED_HOSTS = os.getenv(
