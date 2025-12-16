c2e1560e8e664818b6bb57ba0717e707d0e9fd09
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-16 15:22:04 +0100

    move privacy policy from root to home app

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('privacy/', views.privacy_policy, name='privacy'),
+    path('terms/', views.terms_and_conditions, name='terms'),
 ]
