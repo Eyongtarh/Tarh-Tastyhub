ae9bb61227c5b46efb1aebe23cde1e631bb59441
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-17 20:41:03 +0100

    s3 loading staticfiles

--- tarh_tastyhub/settings.py
+++ tarh_tastyhub/settings.py
@@ -11,7 +11,7 @@ from django.contrib.messages import constants as messages
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Debug
-DEBUG = True
+DEBUG = False
 
 # Allowed hosts
 ALLOWED_HOSTS = os.getenv(
