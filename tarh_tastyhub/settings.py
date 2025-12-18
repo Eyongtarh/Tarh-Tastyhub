3dc04c4e69c36d1dff5c44f8abe6a4f154f6e6a0
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-18 22:58:19 +0100

    fix custom_storages.py

--- tarh_tastyhub/settings.py
+++ tarh_tastyhub/settings.py
@@ -160,6 +160,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 USE_AWS = 'USE_AWS' in os.environ
 
 if USE_AWS:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }
     AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
