1977f4d5f7ae225e970c29f01b815bc57bca0503
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-18 01:45:27 +0100

    fix AWS settings

--- tarh_tastyhub/custom_storages.py
+++ tarh_tastyhub/custom_storages.py
@@ -1,13 +1,14 @@
-from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
 
 
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = STATICFILES_LOCATION
     default_acl = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
-    default_acl = 'public-read'
+    location = MEDIAFILES_LOCATION
     file_overwrite = False
