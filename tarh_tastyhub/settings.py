c625b45fff0742eeeb8424583f7ab87a8d4b3ee0
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-19 05:17:02 +0100

    Fix S3 domain for eu-north-1 bucket

--- tarh_tastyhub/settings.py
+++ tarh_tastyhub/settings.py
@@ -159,20 +159,22 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 if 'USE_AWS' in os.environ:
 
-    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'tarh-tasty-hub'
     AWS_S3_REGION_NAME = 'eu-north-1'
+
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static and media files
+    AWS_S3_CUSTOM_DOMAIN = (
+        f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+    )
+
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
+
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
-    # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
