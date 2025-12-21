3879b40cd8a3c22f4b820ac453b4cabc436e9ae6
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-21 17:29:55 +0100

    edit templates and setup for 403 and 500 errors, browse button in card.html

--- tarh_tastyhub/urls.py
+++ tarh_tastyhub/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from .views import handler404
+from .views import handler403, handler404, handler500
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -35,4 +35,6 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+handler403 = 'tarh_tastyhub.views.handler403'
 handler404 = 'tarh_tastyhub.views.handler404'
+handler500 = 'tarh_tastyhub.views.handler500'
