f3a9dbe500be970e58dc8b81905245014a0125d9
Eyongtarh Besong <eyongtarhb@yahoo.co.uk>
2025-12-21 09:41:00 +0100

    edit custom_storages

--- custom_storages.py
+++ custom_storages.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.contrib.staticfiles.storage import ManifestFilesMixin
 
 
-class StaticStorage(S3Boto3Storage):
+class StaticManifestStorage(ManifestFilesMixin, S3Boto3Storage):
     location = 'static'
+    default_acl = 'public-read'
 
 
 class MediaStorage(S3Boto3Storage):
     location = 'media'
+    default_acl = 'public-read'
     file_overwrite = False
